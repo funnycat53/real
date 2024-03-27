f46bfd8d1e87cf92c80a3f2301cd95d33992dc2f
funnycat53 <patriks.jkb@gmail.com>
2024-03-27 13:21:07 +0200

    Update trenins.py

--- trenins.py
+++ trenins.py
@@ -7,7 +7,7 @@ def rezultats(sk1, sk2):
 
 for skaitlis in range(1, 11, 2):       #range  - funkcija, kas skaita skaitļus
     for otrs in range(2, 11, 2):
-        print("pirmais skaitlis:", skaitlis,"otris skaitlis:", otrs, "rezultāts:", rezultats(skaitlis, otrs))
+        print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts:", rezultats(skaitlis, otrs))
 
 skaitlis1 = 7
 skaitlis2 = 4
@@ -15,3 +15,24 @@ skaitlis2 = 4
 print("pirmais skaitlis:", skaitlis1)
 print("otris skaitlis:", skaitlis2) 
 print("rezultats:", rezultats(skaitlis1, skaitlis2))
+
+
+saraksts1 = [1, 7, 5, 9, 35, 2]
+saraksts2 = [4, 2, 2, 39, 6, 4]
+
+for skaititajs in range(len(saraksts1)):
+    print("skaititajs:", skaititajs, "pirmais skaitlis:", saraksts1[skaititajs],"otrais skaitlis:", saraksts2[skaititajs], "rezultāts:", rezultats(saraksts1[skaititajs], saraksts2[skaititajs]))
+
+skaitlu_pari = [[2,5], [4,7], [3,4], [7,9]]
+
+print("------------------------------------------------")
+
+for i in range(len(skaitlu_pari)):
+    print("skaititajs:", i, "pirmais skaitlis:", skaitlu_pari[i][0],"otrais skaitlis:", skaitlu_pari[i][1], "rezultāts:", rezultats(skaitlu_pari[i][0], skaitlu_pari[i][1]))
+
+print("------------------------------------------------")
+
+for elements in skaitlu_pari:
+     print("pirmais skaitlis:", elements[0],"otrais skaitlis:", elements[1], "rezultāts:", rezultats(elements[0], elements[1]))
+
+
